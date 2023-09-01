5055c7273d39babdf2877c18f686498e924f99a3
NemethNorbert <norbert.nemeth@liferay.com>
2023-09-01 13:14:47 +0200

    LPS-195138 Revert "LPS-174464 SF"
    This reverts commit 2b838a3ceaa99560fc9f109ffaaa3327879b0aba.

--- modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
+++ modules/apps/frontend-js/frontend-js-loader-modules-extender/src/main/java/com/liferay/frontend/js/loader/modules/extender/internal/servlet/JSResolveModulesServlet.java
@@ -9,7 +9,6 @@ import com.liferay.frontend.js.loader.modules.extender.internal.configuration.De
 import com.liferay.frontend.js.loader.modules.extender.internal.resolution.BrowserModulesResolution;
 import com.liferay.frontend.js.loader.modules.extender.internal.resolution.BrowserModulesResolver;
 import com.liferay.frontend.js.loader.modules.extender.npm.NPMRegistryUpdatesListener;
-import com.liferay.petra.string.StringBundler;
 import com.liferay.petra.string.StringPool;
 import com.liferay.portal.kernel.servlet.HttpHeaders;
 import com.liferay.portal.kernel.util.ContentTypes;
@@ -81,15 +80,21 @@ public class JSResolveModulesServlet
 				_absolutePortalURLBuilderFactory.getAbsolutePortalURLBuilder(
 					httpServletRequest);
 
+			StringBuilder sb = new StringBuilder();
+
+			sb.append(
+				absolutePortalURLBuilder.forServlet(
+					getURL()
+				).build());
+
+			sb.append(StringPool.QUESTION);
+
+			sb.append(httpServletRequest.getQueryString());
+
 			// Send a redirect so that the AMD loader knows that it must update
 			// its resolvePath to the new URL.
 
-			httpServletResponse.sendRedirect(
-				StringBundler.concat(
-					absolutePortalURLBuilder.forServlet(
-						getURL()
-					).build(),
-					StringPool.QUESTION, httpServletRequest.getQueryString()));
+			httpServletResponse.sendRedirect(sb.toString());
 
 			return;
 		}
