a98fae20b751cf5eba320cc1e0674f7ed5e93e48
Fortunato <fortunato.maldonado@liferay.com>
2024-05-23 10:50:22 -0600

    LPD-26435 Add playwright test for wiki-web

--- modules/test/playwright/playwright.config.ts
+++ modules/test/playwright/playwright.config.ts
@@ -42,6 +42,7 @@ import {config as productNavigationUserPersonalBarWebConfig} from './tests/produ
 import {config as stableConfig} from './tests/stable/config';
 import {config as stylebookConfig} from './tests/style-book-web/config';
 import {config as usersAdminWebConfig} from './tests/users-admin-web/config';
+import {config as wikiWebConfig} from './tests/wiki-web/config';
 
 const setupProjects = [wemSiteSetup, wemSiteTeardown];
 
@@ -87,6 +88,7 @@ export default defineConfig({
 		stableConfig,
 		stylebookConfig,
 		usersAdminWebConfig,
+		wikiWebConfig,
 		...setupProjects,
 	],
 	reporter: [
